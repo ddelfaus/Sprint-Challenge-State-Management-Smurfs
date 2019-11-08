0380dfb5ad8ac8dfd282d92b2428aafb5e896c83
ddelfaus <ddelfaus@gmail.com>
2019-11-08 12:01:50 -0500

    got local server data to display

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Smurf = props => {
+    console.log(props, "feaeaw")
+
+  return (
+  
+  
+  <div>
+      <h2>{props.smurf.name}</h2>
+      <p>Age: {props.smurf.age}</p>
+      <p>Height: {props.smurf.height}</p>
+
+
+
+  </div>
+
+  )
+};
+
+export default Smurf;
--- smurfs/src/components/Smurfllist.js
+++ smurfs/src/components/Smurfllist.js
@@ -2,21 +2,31 @@ import React from 'react';
 import { useEffect } from 'react'
 import { connect} from 'react-redux'
 
+import Smurf from './Smurf'
+import { fetchSmurfs } from './Actions'
+
+
 
 
 const Smurflist = (props) => {
-
-
-
-
-return (
-    <div>
-
-
-
-    </div>
-)
-
+ 
+    useEffect(() => {
+        props.fetchSmurfs();
+      }, []);
+    
+      if (props.isFetching) {
+        
+        return <h2>Loading Cat Facts...</h2>;
+      }
+    
+    return (
+        <div>
+            {props.error && <p>{props.error}</p>}
+            {props.smurfs.map(item => (
+        <Smurf  key ={item.id} smurf ={item} />
+      ))} 
+        </div>
+    )
 }
 
 const mapStateToProps = state => {
@@ -27,4 +37,4 @@ const mapStateToProps = state => {
 }
 
 
-export default connect(mapStateToProps, {})(Smurflist);
+export default connect(mapStateToProps, {fetchSmurfs})(Smurflist);
